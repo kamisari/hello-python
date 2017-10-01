492b223cffd1d57f2996a3a505ef321e12fe292b
kamisari <kamisari10yonaga@gmail.com>
2017-10-01 14:52:12 +0900

    add def.py  Changes to be committed: 	new file:   def/def.py

---
+++ def/def.py
@@ -0,0 +1,11 @@
+# vim: fileencoding=utf-8
+
+
+def main():
+    '''this is documentation for this function
+    see in CLI `pydoc ./def.py'
+    '''
+    pass
+
+if __name__ == '__main__':
+    main()
