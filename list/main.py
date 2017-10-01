aff97b024d8998ff9c49f6175f43a9bd3aa5f458
kamisari <kamisari10yonaga@gmail.com>
2017-10-01 22:49:05 +0900

    check  Changes to be committed: 	modified:   main.py

--- list/main.py
+++ list/main.py
@@ -16,18 +16,42 @@ def type_list():
 def slice():
     l = [1,2,3,4]
     print(l)
-    print("l[0:]", l[0:])
-    print("l[1:]", l[1:])
-    print("l[:0]", l[:0])
+    print('l[0:]', l[0:])
+    print('l[1:]', l[1:])
+    print('l[:0]', l[:0])
 
     l2 = [1]
-    print("l2[0:]", l2[0:])
-    print("l2[1:]", l2[1:])
+    print('l2[0:]', l2[0:])
+    print('l2[1:]', l2[1:])
+
+def methods():
+    print('list methods:')
+    l = []
+    show = lambda s='': print(':', s, l)
+    l.append(0)
+    show('after append')
+    l.clear()
+    show('after clear')
+    l.append(0)
+    l.append(1)
+    l.reverse()
+    show('after reverse')
+    l.pop()
+    show('after pop')
+    l.append(0)
+    show()
+    l.pop(1)
+    show('after pop(1)')
+    l.extend(l.copy())
+    show('after extend(copy())')
+    l.insert(1, 2)
+    show('after insert(1, 2)')
 
 def main():
     type_list()
-    print("slice")
+    print('slice')
     slice()
 
 if __name__ == '__main__':
     main()
+    methods()
