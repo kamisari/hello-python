a205ece7662dc0ab6d26ae005657f87acc569d80
kamisari <kamisari10yonaga@gmail.com>
2017-10-01 12:28:50 +0900

    add heap.py  Changes to be committed: 	modified:   heap.py

--- sort/heap.py
+++ sort/heap.py
@@ -5,7 +5,32 @@
 
 import numpy as np
 
+# zero baseed heap in array
+# parent:      (index-1)//2
+# child left:  index*2+1
+# child right: index*2+2
+
 def heap(arr):
+    return arr
+
+def makeHeap(arr):
+
+    #i = 0
+    #index = i
+    #x = arr[index]
+    #lam = lambda: x < arr[(index-1)//2]
+    #print(lam())
+
+    for i in range(len(arr)):
+        index = i
+        x = arr[index]
+        print(i, ": before", arr)
+        while index > 0 and x < arr[(index-1)//2]: # reverse  x > arr[(index-1)//2]
+            print("swapp: index=", index, "x=", x, "parent=", arr[(index-1)//2])
+            arr[index] = arr[(index-1)//2]
+            index = (index-1)//2
+        arr[index] = x
+        print(i, ": after ", arr)
     return arr
 
 def main():
@@ -14,6 +39,8 @@ def main():
     afterarr = heap(arr.copy())
     print("after:", afterarr, "afterarr")
     print("check:", arr, "arr")
+
+    print("makeHeap:", makeHeap(arr))
 
 if __name__ == '__main__':
     main()
