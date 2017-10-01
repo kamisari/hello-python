fe143bc3f76df2c5660c5d45fa92b959495c1d61
kamisari <kamisari10yonaga@gmail.com>
2017-10-01 14:52:28 +0900

    up hello.py  Changes to be committed: 	modified:   hello/hello.py

--- hello/hello.py
+++ hello/hello.py
@@ -6,6 +6,15 @@ def main():
     print('hello world')
     print('hello ', end=',,,')
     print('world')
+    print('up\ndown')
+    print(r'up\ndown')
+    s = '''
+multi line
+hello world
+'''
+    print(s)
+    print('dock', 'hello''world')
+    print('hi'*2)
 
 if __name__ == '__main__':
     main()
